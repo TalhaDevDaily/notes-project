047853cc942b25804a87adf00d18b585c9aa5fc8
TalhaDevDaily <talha@boishakhikunja.onmicrosoft.com>
2025-11-12 07:05:49 +0600

    HomeWork 58 - Part 1 done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getDatabase, push, ref, set, onValue } from "firebase/database";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [inputData, setInputData] = useState("");
+
+  const [result, setResult] = useState([]);
+
+  const db = getDatabase();
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    set(push(ref(db, "notesCollection/")), {
+      data: inputData,
+    });
+  };
+
+  // CRUD Operation
+  // C => Create
+  // R => Read
+  // U => Update
+  // D => Delete
+
+  useEffect(() => {
+    const userNotes = ref(db, "notesCollection/");
+    onValue(userNotes, (snapshot) => {
+      let convertArray = [];
+      snapshot.forEach((item) => {
+        // console.log(item.val());
+        convertArray.push(item.val());
+      });
+
+      // console.log(data);
+      setResult(convertArray);
+    });
+  }, []);
+
+  // console.log(result);
+
+  return (
+    <>
+      <form>
+        <input
+          onChange={(e) => setInputData(e.target.value)}
+          type="text"
+          className="bg-black text-white"
+        />
+        <button onClick={handleSubmit}>Add</button>
+      </form>
+      {result.map((item, i) => (
+        <h1 key={i}>{item.data}</h1>
+      ))}
+    </>
+  );
 };
 
 export default Home;
